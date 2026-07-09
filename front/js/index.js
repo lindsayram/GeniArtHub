175c3a7e025646b517ca459e1f922160cf8be195
lindsayram <lindsayrpro@gmail.com>
2026-07-09 14:56:34 +0200

    get id and some informations are ok

--- front/js/index.js
+++ front/js/index.js
@@ -1,5 +1,4 @@
 //Récupérer les images
-
 async function recupImages() {
     try{
         const req = await fetch("http://localhost:3000/api/products/")
@@ -10,15 +9,15 @@ async function recupImages() {
     
     res.forEach(product => {
         //afficher url
-        console.log(`${product.image}`)
-        //faire une boucle dans HTML pour afficher les images dans HTML
-        //Modifier URL de la src img
+        // console.log(`${product._id}`)
 
+        //faire une boucle pour afficher les images dans HTML
+        //Modifier URL de la src img
         const products = document.querySelector('.products')
         const article =`
             <article>
                 <img src="${product.image}" alt="${product.shorttitle}">
-                <a href="product.html">Buy bird</a>
+                <a href="product.html?id=${product._id}">Buy ${product.shorttitle}</a>
             </article> 
             `
        products.insertAdjacentHTML('beforeend', article)
--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,59 @@
+// Récupérer l'url, pour afficher toutes infos
+// console.log(window.location.href)
+//Récupère les paramètres dans l'URL
+// console.log(window.location.search)
+
+// Récupérer l'id dans l'url
+const nbParam = new URLSearchParams(window.location.search)
+// console.log(nbParam)
+const id = nbParam.get('id')
+// console.log(id)
+
+//comparer avec API
+
+async function afficheProduct() {
+    try{
+        const req = await fetch("http://localhost:3000/api/products/")
+        if(!req.ok) {
+            throw new Error (`Erreur HTTP : Statut : ${req.status}`)
+        }
+        const res = await req.json()
+        
+        let index = 0
+        for(compareId of res){
+            if(id == compareId._id){
+                const article = document.querySelector('article')
+                const figure = `
+                    <figure>
+                        <img src="${compareId.image}" alt="${compareId.shorttitle}">
+                    </figure>`
+                article.insertAdjacentHTML('afterbegin', figure)
+
+            // console.log(recherche._id)
+                const div = document.querySelector('#details')
+                const infosArt = `
+                    <h1>${compareId.titre}</h1>
+                    <p>Plongez dans l'univers mystique de 'Bird', une œuvre d'art captivante qui transcende les limites de la réalité. Réalisée dans le style éthéré et spectral, cette pièce évoque la présence d'un oiseau envoûtant qui semble flotter dans l'au-delà.</p>
+                    <div class="price">
+                        <p>Acheter pour</p>
+                        <span class="showprice">35.25€</span>
+                    </div>
+                    <div class="declinaison">
+                        <input type="number" name="quantity" id="quantity" placeholder="1" value="1" minlength="1">
+                        <select name="format" id="format">
+                            
+                        </select>
+                    </div>
+                    <a class="button-buy" href="#">Buy ${compareId.shorttitle}</a>`
+                div.insertAdjacentHTML('afterbegin', infosArt)
+            }
+        }
+    }catch(error) {
+        console.error(`Une erreur est survenue : ${error}`)
+    }
+}
+
+afficheProduct()
 //Liste SELECT
 // function afficher() {
 //     const liste = document.querySelector("select");
